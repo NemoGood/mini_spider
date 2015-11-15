0d7e33c1ba918564178953761d896215350ebadf
NemoGood <aplle@apllematoMacBook-Pro.local>
2015-11-15 23:39:44 +0800

    remove test file
    remove the test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import argparse
-#test
-parser = argparse.ArgumentParser()
-parser.add_argument('-c','--filename',help="the file of the conf")
-args = parser.parse_args()
-argfile = args.filename
-print(argfile)
